bbe5b0031162dbb2ac4c8249457bfc42f1b7e081
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2024-09-11 14:31:34 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+	"./src/**/*.{js,jsx}"
+  ],
+  prefix: "",
+  theme: {
+	container: {
+		center: true,
+		padding: "15px",
+	},
+	screens: {
+		sm: '640px',
+		md: '768px',
+		lg: '960px',
+		xl: '1200px',
+	},
+	fontFamily: {
+		primary: "var(--font-jetbrainsMono)",
+	},
+  	extend: {
+  		colors: {
+  			primary: '#1c1c22',
+			accent: {
+				DEFAULT: '#00ff99',
+				hover: "#00e187",
+			},
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
