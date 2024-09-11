6dc4e3fa945015f058a6ccdf80a3f0e31fcb0834
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2024-09-11 16:49:30 +0500

    initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,9 +3,10 @@ import "./globals.css";
 
 // components
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800"], 
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   variable: '--font-jetbrainsMono'
 });
 
@@ -15,12 +16,15 @@ export const metadata = {
 };
 
 
-export default function RootLayout({ children }) { 
+export default function RootLayout({ children }) {
   return (
-  <html lang="en">
-    <body className={jetbrainsMono.variable}>
-      <Header />
-      {children}</body>
-  </html>
+    <html lang="en">
+      <body className={jetbrainsMono.variable}>
+        <Header />
+        <PageTransition>
+          {children}
+        </PageTransition>
+      </body>
+    </html>
   );
 }
--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,11 +1,5 @@
-import React from 'react'
+const PageTransition = ({ children }) => {
+    return <AnimatePresence>{children}</AnimatePresence>;
+};
 
-const PageTransition = () => {
-    return (
-        <div>
-
-        </div>
-    )
-}
-
-export default PageTransition
+export default PageTransition;
