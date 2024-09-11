4d6cb69aac450cb7b90d58d1dfccfc2821549089
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2024-09-11 19:31:40 +0500

    changes made

---
+++ components/MobileNav.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { usePathname } from 'next/navigation';
+
+
+const MobileNav = () => {
+    return <nav>MobileNav</nav>;
+};
+
+export default MobileNav;
