2fa71c45ae43b3bbe8262eb8578ae53b298f5b02
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2024-09-11 21:29:36 +0500

    changes Made

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,39 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi"
+
+//components
+import Social from "@/components/Social";
+const Home = () => {
   return (
-    <main>homepage</main>
+    <section className="h-full">
+      <div className="container h-full mx-auto">
+        <div className="flex flex-col items-center justify-between xl:flex-row xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1">
+              Hello I'm <br /> <span className="text-accent">Luke Coleman</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting digital experiences and I am proficient in various programming languages and technologies.
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col items-center gap-8 xl:flex-row">
+              <Button variant="outline" size="lg" className="flex items-center gap-2 uppercase">
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
---
+++ components/Social.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+import { FaGithub, FaLinkedIn, FaYoutube, FaTwitter } from "react-icons/fa"
+
+const socials = [
+    { icon: <FaGithub />, patn: '' },
+    { icon: <FaLinkedIn />, patn: '' },
+    { icon: <FaYoutube />, patn: '' },
+    { icon: <FaTwitter />, patn: '' },
+]
+
+const Social = () => {
+    return <div>Social</div>;
+};
+
+export default Social;
