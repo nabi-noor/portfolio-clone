08eee71a0d433eff14997bfe45b4cfd67ab59b5b
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2024-09-12 12:57:14 +0500

    changes made

--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,14 @@ import { FiDownload } from "react-icons/fi"
 //components
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/stats";
 const Home = () => {
   return (
     <section className="h-full">
       <div className="container h-full mx-auto">
         <div className="flex flex-col items-center justify-between xl:flex-row xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1">
               Hello I'm <br /> <span className="text-accent">Luke Coleman</span>
@@ -30,11 +31,12 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
