225b959144b06f7fa3344a14d352b19b4d22c8ad
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2024-09-15 11:46:13 +0500

    changes made

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -2,8 +2,8 @@
 
 import { motion } from "framer-motion";
 import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import Link from "next/link";
@@ -42,7 +42,19 @@ const projects = [
     },
 ]
 const Work = () => {
-    return <div>work page</div>;
+    const [project, setProject] = useState(projects[0])
+    return (
+        <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+                    <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+                        <div></div>
+                    </div>
+                    <div className="w-full xl:w-[50%]">slider</div>
+                </div>
+            </div>
+        </motion.section>
+    );
 };
 
 export default Work;
