afcc44622cc2c17e4aa3f6466f9653c8b5f11d1d
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2024-09-17 21:39:46 +0500

    responsiveness issues solved

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -25,11 +25,11 @@ const Stats = () => {
     return (
         <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
             <div className="container mx-auto">
-                <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+                <div className="flex flex-col flex-nowrap gap-6 max-w-[80vw] mx-auto xl:max-w-none xl:flex-row lg:flex-row">
                     {stats.map((item, index) => {
                         return (
                             <div
-                                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+                                className="flex items-center justify-center flex-1 gap-4 xl:justify-start"
                                 key={index}
                             >
                                 <CountUp
